beef6cb7c849bcc06680d0cc100b76565f1181c4
patxi <patxiarana05@gmail.com>
2023-12-24 08:36:52 -0300

    autoload con composer

--- IntroduccionPHPOOP_Inicio/08.php
+++ IntroduccionPHPOOP_Inicio/08.php
@@ -4,27 +4,65 @@
 
 require "clases/Detalle.php";*/
 
-
+require 'vendor/autoload.php'; 
 
 
 
 use App\Clientes ;
 use App\Detalles ; 
-
-
-
-//incluir las otras clases
-function mi_autoload($clase)  {
- $partes = explode('\\', $clase ) ;
-    require __DIR__ . "/clases/" . $partes[1] . ".php"  ; 
-}
-
-spl_autoload_register('mi_autoload') ; 
-
-require 'clases/Clientes.php' ;
-require 'clases/Detalles.php' ;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+//require 'clases/Clientes.php' ;
+//require 'clases/Detalles.php' ;
 
 
 $detalles = new Detalles() ; 
 $clientes = new Clientes() ; 
+
+$key = 'example_key';
+$payload = [
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
+    'iat' => 1356999524,
+    'nbf' => 1357000000
+];
+
+/**
+ * IMPORTANT:
+ * You must specify supported algorithms for your application. See
+ * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+ * for a list of spec-compliant algorithms.
+ */
+$jwt = JWT::encode($payload, $key, 'HS256');
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+print_r($decoded);
+
+// Pass a stdClass in as the third parameter to get the decoded header values
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'), $headers = new stdClass());
+print_r($headers);
+
+/*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+
+$decoded_array = (array) $decoded;
+
+/**
+ * You can add a leeway to account for when there is a clock skew times between
+ * the signing and verifying servers. It is recommended that this leeway should
+ * not be bigger than a few minutes.
+ *
+ * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
+ */
+JWT::$leeway = 60; // $leeway in seconds
+$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+
+
+
+
+
+
+
+
 include 'includes/footer.php';
---
+++ IntroduccionPHPOOP_Inicio/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'usuario/introduccion-phpoop_inicio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dfa8d249947ec557e4b41aa3fad7b81813ef0403',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'a49db6f0a5033aef5143295342f1c95521b075ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'usuario/introduccion-phpoop_inicio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dfa8d249947ec557e4b41aa3fad7b81813ef0403',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
