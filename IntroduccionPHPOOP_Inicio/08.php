dfa8d249947ec557e4b41aa3fad7b81813ef0403
patxi <patxiarana05@gmail.com>
2023-12-24 08:12:56 -0300

    namespaces

--- IntroduccionPHPOOP_Inicio/08.php
+++ IntroduccionPHPOOP_Inicio/08.php
@@ -4,12 +4,26 @@
 
 require "clases/Detalle.php";*/
 
+
+
+
+
+use App\Clientes ;
+use App\Detalles ; 
+
+
+
+//incluir las otras clases
 function mi_autoload($clase)  {
- 
-    require __DIR__ . "/clases/" . $clase . ".php"  ; 
+ $partes = explode('\\', $clase ) ;
+    require __DIR__ . "/clases/" . $partes[1] . ".php"  ; 
 }
 
 spl_autoload_register('mi_autoload') ; 
+
+require 'clases/Clientes.php' ;
+require 'clases/Detalles.php' ;
+
 
 $detalles = new Detalles() ; 
 $clientes = new Clientes() ; 
